655741a3e14608b053fad09142e3f81fd995b3a6
emmanhaider <emmanhaider@gmail.com>
2015-06-27 04:59:58 -0400

    Update myo_enghack.js

--- myo_enghack.js
+++ myo_enghack.js
@@ -1,3 +1,6 @@
+var Myo = require('myo');
+var myMyo = Myo.create();
+
 if (isLocked) document.write("Myo is LOCKED")
 else {
   document.write("Myo is UNLOCKED")
@@ -6,6 +9,8 @@ myo.on('fist', function(edge){
     //Edge is true if it's the start of the pose, false if it's the end of the pose
     if(edge){
          myo.zeroOrientation();
+         console.log('You may start your exercise');
+         myMyo.vibrate('short');
     }
 });
 document.write(myo.orientationOffset)

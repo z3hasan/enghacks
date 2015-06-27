74ca89c7105a653aa95bbed15955e0b12324a514
Zara <z3hasan@outlook.com>
2015-06-26 22:56:28 -0400

    Remove other graphs, added new file

---
+++ myo_enghack.js
@@ -0,0 +1,7 @@
+myMyo.on('fist', function(edge){
+    //Edge is true if it's the start of the pose, false if it's the end of the pose
+    if(edge){
+        enemies.crush();
+    }
+});
+alert ('sug');
